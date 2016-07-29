fa310229b312448522179a01de0b430d23e89259
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-07-29 08:45:11 -0400

    wired in search form

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+$('#search-term').submit(function(e) {
+	e.preventDefault();
+	var userInput = $('#user-input').val();
+	getVideos(userInput);
+});
 
-
-function getVideos(){
+function getVideos(searchedFilm){
 	var link = "https://www.tastekid.com/api/similar?callback=?";
 	var params = {
 		type: 'movie',
 		k: '233921-Movieapp-160TW6HV',
-		q: "The Shining"		
+		q: searchedFilm		
 	};
 	$.getJSON(link,params, function (data) {
 		console.log(data);
@@ -24,32 +28,10 @@ function getVideos(){
     		var listNames = movies[i].Name;
     		thumbnail += '<li>' + listNames + '</li>';
     	}
-    	
+
     $('#results').html(thumbnail);
     $('#results').append(thumbnail);
   };
 
-getVideos();
-///
-// create random video channels
-// 	function display(random) {
-// 		var html = '';
 
-// 		$.each(random, function (index, video) {
-// 			console.log(video);
-// 			html = html + "<li><p>" + video.snippet.title + "</p>"
-// 				+"<a class='image' href='https://www.youtube.com/watch?v="+video.id.videoId+ "'>"
-// 					+"<img src='" +  video.snippet.thumbnails.high.url + "' height=300px/></a></li>" ;
 
-// 		});
-// 		$("#content").html(html);
-// 	}
-
-// 	getResults(random());
-
-// function random() {
-// 	var searches = ['cats', 'dogs', 'mice','pokemon', 'monga', 'game', 'music', 'pop'];
-// 		var randomN = Math.floor(Math.random() * searches.length);
-// 		return searches[randomN];		
-
-// }
